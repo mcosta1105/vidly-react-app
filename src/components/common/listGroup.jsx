9f43926cac79923bbedc227c1785b01a82be93ce
Maycon <maycon@wordflow.info>
2019-06-17 17:00:13 +1000

    Extracted table components and implemented sorting by columns

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const ListGroup = props => {
-  const { items, valueProperty, textProperty, onItemSelect, selectedItem } = props;
+const ListGroup = ({ items, valueProperty, textProperty, onItemSelect, selectedItem }) => {
   return (
     <ul className="list-group">
       {items.map(item => (
         <li
           key={item[valueProperty]}
-          className={item === selectedItem ? "list-group-item active":"list-group-item"}
+          className={item === selectedItem ? "clickable list-group-item active":"clickable list-group-item"}
           onClick={() => onItemSelect(item)}
         >
           {item[textProperty]}

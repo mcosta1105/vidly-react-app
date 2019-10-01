a1854c0346eb5eea949ac587d3816bfe98366bbd
Maycon <costa.maycon@live.com>
2019-10-02 00:00:23 +1000

    login implemented

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -20,9 +21,20 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {
-    //call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      //call the server
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem('token', jwt);
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const { errors } = this.state;
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {

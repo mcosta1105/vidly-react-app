77fff99230188f7c7d9d35d06a28c6e7aa1c2cec
Maycon <costa.maycon@live.com>
2019-10-20 22:12:57 +1100

    Call protected api implemented

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,6 +6,8 @@ import jwtDecode from "jwt-decode";
 const apiEndPoint = apiUrl + "/auth";
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login (email, password){
     const {data:jwt} = await http.post(apiEndPoint, {email, password});
     localStorage.setItem(tokenKey, jwt);
@@ -28,9 +30,14 @@ export function getCurrentUser(){
       }
 }
 
+export function getJwt(){
+    return localStorage.getItem(tokenKey);
+}
+
 export default {
     login,
     logout, 
     getCurrentUser,
-    loginWithJwt
+    loginWithJwt,
+    getJwt
 }

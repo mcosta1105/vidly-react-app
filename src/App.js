235062ccd872ef90d7726626d7280e38e45fe0f5
Maycon <costa.maycon@live.com>
2019-10-21 23:06:46 +1100

    Route protection implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Movies from "./components/movies";
@@ -10,29 +10,34 @@ import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import Logout from "./components/logout";
+import ProtectedRoute from "./components/common/protectedRoute";
 import auth from "./services/authService";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
   state = {};
-  
-  componentDidMount(){
+
+  componentDidMount() {
     const user = auth.getCurrentUser();
-    this.setState({user});
+    this.setState({ user });
   }
   render() {
+    const { user } = this.state;
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar user={this.state.user} />
+        <NavBar user={user} />
         <main className="container">
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={props => <Movies {...props} user={user} />}
+            />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -96,6 +96,7 @@ class Movies extends Component {
   render() {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const {user} = this.props;
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
@@ -110,9 +111,9 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link className="btn btn-primary mb-2" to="/movies/new">
+          {user && (<Link className="btn btn-primary mb-2" to="/movies/new">
             New Movie
-          </Link>
+          </Link>)}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
